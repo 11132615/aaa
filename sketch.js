684935d4b4884361d41c5eb4f28cd39fac1114b5
11132615 <145106110+11132615@users.noreply.github.com>
2024-12-03 19:16:23 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://https://11132615.github.io/aaa/model.json';
 
 
 function preload() {
